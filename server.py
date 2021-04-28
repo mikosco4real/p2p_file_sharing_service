c502884109337078cd2a1c57a3d5f6754e782bb7
Michael Okolo <okolomikel2009@gmail.com>
2021-04-28 23:27:30 +1000

    Initialised the server and client files

---
+++ server.py
@@ -0,0 +1,15 @@
+# Add Peers to the network and broadcast the peer details to all peers
+import os
+import socket
+from threading import Thread
+import unittest
+
+class Server:
+    pass
+
+
+class TestServer(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
